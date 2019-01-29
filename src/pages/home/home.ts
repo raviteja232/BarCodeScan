c1ec8791cd3fcededd06b40178392f188862d6e6
Ravi Teja M <raviteja@rjy.pllcsoftwaresolutions.com>
2019-01-29 19:45:07 +0530

    New Files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { BarcodeScanner,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+options:BarcodeScannerOptions;
+encodText:string='';
+encodedData:any={};
+scannedData:any={};
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public scanner:BarcodeScanner) {
 
   }
-
+  scan(){
+    this.options={
+      prompt:'Scan your Barcode'
+    };
+    this.scanner.scan(this.options).then((data)=>{
+        this.scannedData=data;
+    },(err)=>{
+      console.log('Err : ',err);
+    })
+  }
+  encode(){
+    this.scanner.encode(this.scanner.Encode.TEXT_TYPE,this.encodText).then((data)=>{
+        this.encodedData=data;
+    },(err)=>{})
+  }
 }
